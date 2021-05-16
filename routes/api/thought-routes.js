e77c8c45910b3e831627f9173e3e37ab862775c4
Paden Allen <padenallen15@yahoo.com>
2021-05-16 12:39:15 -0500

    done

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -22,13 +22,5 @@ router
 // /api/thoughts/<userId>
 router.route('/:userId').post(addThought);
 
-// /api/thoughts/<userId>/<thoughtId>
-// router
-//   .route('/:userId/:thoughtId')
-//   //.put(addReply)
-//   .delete(removeThought);
-
-// /api/thoughts/<userId>/<thoughtId>/<replyId>
-//router.route('/:userId/:thoughtId/:replyId').delete(removeReply);
 
 module.exports = router;
